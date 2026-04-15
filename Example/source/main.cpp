66cd87e181b1305c0868bd4cf555fc8c7725c50f
Francisco <yostarduck@gmail.com>
2026-04-15 04:22:03 -0600

    * Added miscellaneous files to gitignore

--- Example/source/main.cpp
+++ Example/source/main.cpp
@@ -6,5 +6,30 @@ int32_t
 main(int32_t argc, char** argv) {
   std::cout << "Hello World!" << std::endl;
 
+  SFW::WindowCreateInfo createInfo;
+  createInfo.title = "SFW Example";
+  createInfo.x = 0;
+  createInfo.y = 0;
+  createInfo.width = 1080;
+  createInfo.height = 720;
+  createInfo.visible = true;
+  createInfo.resizable = false;
+  createInfo.decorated = true;
+
+  SFW::Window* window = SFW::Window::create(createInfo);
+
+  if (window == nullptr) {
+    std::cerr << "Failed to create window!" << std::endl;
+    return -1;
+  }
+
+  while (!window->shouldClose()) {
+    window->pollEvents();
+  }
+
+  window->destroy();
+
+  delete window;
+
   return 0;
 }
--- SFW/include/SFW_Window.h
+++ SFW/include/SFW_Window.h
@@ -1,21 +1,150 @@
 #pragma once
 
-#if defined(SFW_WINDOWS)
-  #include "SFW_WindowsWindow.h"
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
 
-#elif defined(SFW_LINUX)
-  #if defined(SFW_USE_WAYLAND)
-    //#include "Linux/Wayland/SFW_WaylandWindow.h"
-  #elif defined(SFW_USE_XCB)
-    #include "Linux/XCB/SFW_XCBWindow.h"
-  #else // SFW_USE_X11
-    //#include "Linux/X11/SFW_X11Window.h"
-  #endif
+namespace SFW
+{
 
-#elif defined(SFW_MACOS)
-  #include "SFW_MacOSWindow.h"
+struct WindowCreateInfo
+{
+  std::string title;
 
-#else
-  #error "Unsupported platform"
+  int32_t x {0};
+  int32_t y {0};
 
-#endif
+  uint32_t width {0};
+  uint32_t height {0};
+
+  bool visible {true};
+  bool resizable {true};
+  bool decorated {true};
+};
+
+struct IconData
+{
+  uint32_t width {0};
+  uint32_t height {0};
+
+  std::vector<uint8_t> rgba8;
+};
+
+class Window
+{
+ public:
+	virtual ~Window() = default;
+
+#pragma region Lifecycle
+	static Window*
+	create(const WindowCreateInfo& createInfo);
+
+	virtual bool
+  createInternal(const WindowCreateInfo& createInfo) = 0;
+
+	virtual void
+  destroy() = 0;
+
+	virtual bool
+  isCreated() const = 0;
+
+  virtual void*
+	getNativeHandle() const = 0;
+#pragma endregion
+
+#pragma region Event processing
+	virtual void
+  pollEvents() = 0;
+
+	virtual void
+  requestClose() = 0;
+
+	virtual bool
+  shouldClose() const = 0;
+#pragma endregion
+
+#pragma region Visibility and focus
+	virtual void
+  show() = 0;
+
+	virtual void
+  hide() = 0;
+
+	virtual bool
+  isVisible() const = 0;
+
+	virtual void
+  focus() = 0;
+
+	virtual bool
+  hasFocus() const = 0;
+#pragma endregion
+
+#pragma region Position and size
+	virtual void
+  setPosition(const int32_t x, const int32_t y) = 0;
+  
+	virtual void
+  getPosition(int32_t& x, int32_t& y) const = 0;
+  
+	virtual void
+  setSize(const uint32_t width, const uint32_t height) = 0;
+  
+	virtual void
+  getSize(uint32_t& width, uint32_t& height) const = 0;
+
+	virtual void
+  getFramebufferSize(uint32_t& width, uint32_t& height) const = 0;
+#pragma endregion
+
+#pragma region Window state
+	virtual void
+  maximize() = 0;
+
+	virtual void
+  minimize() = 0;
+
+	virtual void
+  restore() = 0;
+
+	virtual bool
+  isMaximized() const = 0;
+
+	virtual bool
+  isMinimized() const = 0;
+#pragma endregion
+
+#pragma region Window attributes
+	virtual void
+	setTitle(const std::string_view title) = 0;
+
+	virtual std::string_view
+  getTitle() const = 0;
+
+	virtual void
+	setResizable(const bool resizable) = 0;
+
+	virtual bool
+  isResizable() const = 0;
+
+	virtual void
+	setDecorated(const bool decorated) = 0;
+
+	virtual bool
+  isDecorated() const = 0;
+  
+  /*
+  virtual bool
+  setIcon(const IconData& icon) = 0;
+  */
+#pragma endregion
+
+ protected:
+  Window() = default;
+
+ private:
+
+};
+
+}
